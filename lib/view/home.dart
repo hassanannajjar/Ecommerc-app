b47b50834e47ab46cdd27d41747a71962bfc1f2a
Hassan Al-Najjar <hassan.an.najjar@gmail.com>
2024-09-17 20:09:30 +0300

    Refactor ViewItem screen layout and add image and button functionality

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../utils/assets.dart';
+import '../utils/routes/route_keys.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -22,141 +23,208 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(
-              left: 20,
-              right: 150,
-            ),
-            child: Text(
-              'Hello Tony, What fruit salad combo do you want today?',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w600,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 20,
+                right: 150,
+              ),
+              child: Text(
+                'Hello Tony, What fruit salad combo do you want today?',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.7,
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.grey,
-                    ),
-                    filled: true,
-                    fillColor: Color(0xffF3F1F1),
-                    hintText: 'Search for fruit salad combos',
-                    hintStyle: TextStyle(
-                      color: Color(0xffB1B1B1),
-                      fontSize: 14,
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.7,
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      ),
+                      filled: true,
+                      fillColor: Color(0xffF3F1F1),
+                      hintText: 'Search for fruit salad combos',
+                      hintStyle: TextStyle(
+                        color: Color(0xffB1B1B1),
+                        fontSize: 14,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              IconButton(
-                icon: SvgPicture.asset(
-                  filterIcon,
+                IconButton(
+                  icon: SvgPicture.asset(
+                    filterIcon,
+                  ),
+                  onPressed: () {},
                 ),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            '  Recommended Combo',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              fontStyle: FontStyle.italic,
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.25,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              children: const [
-                RecommendedItem(),
-                RecommendedItem(),
-                RecommendedItem()
               ],
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Expanded(
-            child: DynamicTabBarWidget(
-              dividerHeight: 0,
-              indicatorColor: Colors.orange,
-              labelColor: Colors.black,
-              unselectedLabelColor: Colors.grey,
-              indicatorSize: TabBarIndicatorSize.tab,
-              indicatorPadding: const EdgeInsets.only(
-                right: 80,
-              ),
-              labelPadding: EdgeInsets.zero,
-              labelStyle: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-              unselectedLabelStyle: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
-              dynamicTabs: [
-                TabData(
-                  index: 1,
-                  title: const Tab(child: Text('Hottest')),
-                  content: Container(
-                    child: const Text('Hottest'),
-                  ),
-                ),
-                TabData(
-                  index: 2,
-                  title: const Tab(child: Text('Popular')),
-                  content: Container(
-                    child: const Text('Popular'),
-                  ),
-                ),
-                TabData(
-                  index: 2,
-                  title: const Tab(child: Text('New combo')),
-                  content: Container(
-                    child: const Text('New combo'),
-                  ),
-                ),
-                TabData(
-                  index: 2,
-                  title: const Tab(child: Text('Top')),
-                  content: Container(
-                    child: const Text('Top'),
-                  ),
-                ),
-              ],
-              onTabControllerUpdated: (tab) {},
+            const SizedBox(
+              height: 20,
             ),
-          )
-        ],
+            const Text(
+              '  Recommended Combo',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.25,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                children: const [
+                  RecommendedItem(),
+                  RecommendedItem(),
+                  RecommendedItem()
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: DynamicTabBarWidget(
+                dividerHeight: 0,
+                indicatorColor: Colors.orange,
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicatorPadding: const EdgeInsets.only(
+                  right: 80,
+                ),
+                labelPadding: EdgeInsets.zero,
+                labelStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                unselectedLabelStyle: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
+                dynamicTabs: [
+                  TabData(
+                    index: 1,
+                    title: const Tab(child: Text('Hottest')),
+                    content: GridView.count(
+                      crossAxisCount: 2,
+                      physics: const NeverScrollableScrollPhysics(),
+                      // mainAxisSpacing: 10,
+                      shrinkWrap: true,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                      ),
+                      cacheExtent: 4,
+                      childAspectRatio: 0.8,
+                      children: const [
+                        RecommendedItem(),
+                        RecommendedItem(),
+                        RecommendedItem(),
+                        RecommendedItem(),
+                      ],
+                    ),
+                  ),
+                  TabData(
+                    index: 2,
+                    title: const Tab(child: Text('Popular')),
+                    content: GridView.count(
+                      crossAxisCount: 2,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+
+                      // mainAxisSpacing: 10,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                      ),
+                      cacheExtent: 4,
+                      childAspectRatio: 0.8,
+                      children: const [
+                        RecommendedItem(),
+                        RecommendedItem(),
+                        RecommendedItem(),
+                        RecommendedItem(),
+                      ],
+                    ),
+                  ),
+                  TabData(
+                    index: 2,
+                    title: const Tab(child: Text('New combo')),
+                    content: GridView.count(
+                      crossAxisCount: 2,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+
+                      // mainAxisSpacing: 10,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                      ),
+                      cacheExtent: 4,
+                      childAspectRatio: 0.8,
+                      children: const [
+                        RecommendedItem(),
+                        RecommendedItem(),
+                        RecommendedItem(),
+                        RecommendedItem(),
+                      ],
+                    ),
+                  ),
+                  TabData(
+                    index: 2,
+                    title: const Tab(child: Text('Top')),
+                    content: GridView.count(
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+
+                      crossAxisCount: 2,
+                      // mainAxisSpacing: 10,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                      ),
+                      cacheExtent: 4,
+                      childAspectRatio: 0.8,
+                      children: const [
+                        RecommendedItem(),
+                        RecommendedItem(),
+                        RecommendedItem(),
+                        RecommendedItem(),
+                      ],
+                    ),
+                  ),
+                ],
+                onTabControllerUpdated: (tab) {
+                  print(tab);
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -165,60 +233,69 @@ class Home extends StatelessWidget {
 class RecommendedItem extends StatelessWidget {
   const RecommendedItem({
     super.key,
+    this.bgColor,
   });
+
+  final Color? bgColor;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.25,
-        width: MediaQuery.of(context).size.width * 0.45,
-        child: Column(
-          children: [
-            Align(
-              alignment: AlignmentDirectional.centerEnd,
-              child: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.favorite_outline,
-                  color: Colors.orange,
-                ),
-              ),
-            ),
-            Image.asset(
-              splashImage,
-              height: 100,
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            const Text(
-              'Fruit Salad Combo',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                const Text(
-                  r'$2000',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.orange,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                IconButton(
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, routeViewItem);
+      },
+      child: Card(
+        color: bgColor,
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.25,
+          width: MediaQuery.of(context).size.width * 0.45,
+          child: Column(
+            children: [
+              Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: IconButton(
                   onPressed: () {},
-                  icon: SvgPicture.asset(
-                    addIcon,
+                  icon: const Icon(
+                    Icons.favorite_outline,
+                    color: Colors.orange,
                   ),
-                )
-              ],
-            )
-          ],
+                ),
+              ),
+              Image.asset(
+                splashImage,
+                height: 100,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                'Fruit Salad Combo',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  const Text(
+                    r'$2000',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.orange,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      addIcon,
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/view/view_item.dart
+++ lib/view/view_item.dart
@@ -1,12 +1,215 @@
 import 'package:flutter/material.dart';
 
+import '../utils/assets.dart';
+
 class ViewItem extends StatelessWidget {
   const ViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('ViewItem Screen'),
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Image.asset(
+              authImage,
+              height: MediaQuery.sizeOf(context).height * 0.3,
+            ),
+          ),
+          Container(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.sizeOf(context).height * 0.576,
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Quinoa Fruit Salad',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    InkWell(
+                                      onTap: () {},
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Ink(
+                                        height: 30,
+                                        width: 30,
+                                        decoration: BoxDecoration(
+                                          color: Colors.grey[300],
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                        ),
+                                        child: const Center(
+                                          child: Icon(
+                                            Icons.remove,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                    const Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 10),
+                                      child: Text(
+                                        '2',
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: () {},
+                                      borderRadius: BorderRadius.circular(20),
+                                      child: Ink(
+                                        height: 30,
+                                        width: 30,
+                                        decoration: BoxDecoration(
+                                          color: Colors.orange,
+                                          borderRadius:
+                                              BorderRadius.circular(20),
+                                        ),
+                                        child: const Center(
+                                          child: Icon(
+                                            Icons.add,
+                                            color: Colors.orange,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const Row(
+                                  children: [
+                                    Text('Price: '),
+                                    Text('N500'),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Divider(
+                              height: 0.5,
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            const Text(
+                              'One Pack Contains:',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w300,
+                                decoration: TextDecoration.underline,
+                                decorationColor: Colors.orange,
+                                decorationStyle: TextDecorationStyle.solid,
+                                decorationThickness: 3,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Text(
+                              'Red Quinoa, Lime, Honey, Blueberries, Strawberries, Mango, Fresh mint.',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Divider(
+                              height: 0.5,
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Text(
+                              'If you are looking for a new fruit salad to eat today, quinoa is the perfect brunch for you. make',
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.favorite_outline,
+                                color: Colors.orange,
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.orange,
+                                minimumSize: Size(
+                                    MediaQuery.sizeOf(context).width * 0.6, 56),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child: const Text(
+                                'Add to basket',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
